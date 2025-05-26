2292b2441f30ebcd8dd37437e408918851d84fc9
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-05-26 09:46:58 -0700

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,21 @@
-
-// import React,{useState} from "react";
-// import './../styles/App.css';
-// import { Tooltip } from "./Tooltip";
-
-// const App = () => {
-
-//   const [text1,setText1]=useState("");
-//   const [text2,setText2]=useState("");
-
-//  function handleOver1(){
-// setText1("This is a tooltip")
-//  }
-
-//  function handleOver2(){
-//   setText2("This is another tooltip")
-  
-//  }
-
-
-//   return (
-//     <div>
-//       <Tooltip  text={text1} />
-//      <h1 style={{height:"200px" ,width:"400px"}} onMouseEnter={()=>setText1("")} onMouseLeave={handleOver1} >Hover Over Me</h1>
-
-//      <h4 onMouseOut={()=>setText2("")}  onMouseOver={handleOver2}>This is another Hover</h4>
-     
-//      <Tooltip text={text2}/>
-//     </div>
-//   )
-// }
-
-// export default App
-// import React, { useState } from 'react'
-// import Tooltip from './Tooltip'
-
-
-// export const App = () => {
- 
-//   return (
-
-//     <div>
-//       <Tooltip text="This is a Tooltip"><h2>Hover over me</h2>
-//       </Tooltip>
-//       <Tooltip text="This is another Tooltip"><h4>Hover over this Paragraph</h4></Tooltip>
-//     </div>
-//   )
-// }
-
-// export default App
-
-import React from 'react';
-import Tooltip from './Tooltip';
+import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
-    return (
-        <div>
-            <Tooltip text="This is a tooltip">
-                <h2>Hover over me</h2>
-            </Tooltip>
-            <Tooltip text="This is another tooltip">
-                <p>Hover over this paragraph</p>
-            </Tooltip>
-        </div>
-    );
-};
 
-export default App;
+
+  return (
+    <div>
+      <h2 className="tooltip">
+        <Tooltip text="This is a tooltip">Hover over me</Tooltip>
+        </h2>
+        <br />
+        <p className="tooltip">
+        <Tooltip text="This is another tooltip">Hover over me to see another tooltip</Tooltip>
+        </p>
+    </div>
+  )
+}
+
+export default App
