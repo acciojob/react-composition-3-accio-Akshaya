ffd0a4e08a2cb5ee347d53edfcc4a173e6fd5767
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-05-26 09:35:44 -0700

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,69 @@
-import React from "react";
-import '../styles/App.css';
-import ToolTip from "./Tooltip";
+
+// import React,{useState} from "react";
+// import './../styles/App.css';
+// import { Tooltip } from "./Tooltip";
+
+// const App = () => {
+
+//   const [text1,setText1]=useState("");
+//   const [text2,setText2]=useState("");
+
+//  function handleOver1(){
+// setText1("This is a tooltip")
+//  }
+
+//  function handleOver2(){
+//   setText2("This is another tooltip")
+  
+//  }
+
+
+//   return (
+//     <div>
+//       <Tooltip  text={text1} />
+//      <h1 style={{height:"200px" ,width:"400px"}} onMouseEnter={()=>setText1("")} onMouseLeave={handleOver1} >Hover Over Me</h1>
+
+//      <h4 onMouseOut={()=>setText2("")}  onMouseOver={handleOver2}>This is another Hover</h4>
+     
+//      <Tooltip text={text2}/>
+//     </div>
+//   )
+// }
+
+// export default App
+// import React, { useState } from 'react'
+// import Tooltip from './Tooltip'
+
+
+// export const App = () => {
+ 
+//   return (
+
+//     <div>
+//       <Tooltip text="This is a Tooltip"><h2>Hover over me</h2>
+//       </Tooltip>
+//       <Tooltip text="This is another Tooltip"><h4>Hover over this Paragraph</h4></Tooltip>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react';
+import Tooltip from './Tooltip';
+import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div className="container">
-      <ToolTip text="This is a tooltip">
-        <h2>Hover over me</h2>
-      </ToolTip>
-      <hr />
-      <ToolTip text="This is another tooltip">
-        <p>Hover over me to see another tooltip</p>
-      </ToolTip>
-    </div>
-  );
+    return (
+        <div>
+            <Tooltip text="This is a tooltip">
+                <h2>Hover over me</h2>
+            </Tooltip>
+            <Tooltip text="This is another tooltip">
+                <p>Hover over this paragraph</p>
+            </Tooltip>
+        </div>
+    );
 };
 
 export default App;
--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,24 +1,42 @@
-import React, { useState, cloneElement } from "react";
+// import React from 'react'
 
-const ToolTip = ({ text, children }) => {
-  const [showTooltip, setShowTooltip] = useState(false);
+// export const Tooltip = (props) => {
+//   return (
+//     <div className='tooltip'><p className='tooltiptext' style={{backgroundColor: props.text.length ==0 ? "white" :"red", fontSize:"30px", color:'white', display:"inline-block", height:"100px", width:"200px",display:"flex",  padding:"20px", alignItems:'center'}} >{props.text}</p></div>
+//   )
+// }
 
-  const handleMouseEnter = () => setShowTooltip(true);
-  const handleMouseLeave = () => setShowTooltip(false);
+// import React, { useState } from 'react'
 
-  // Clone the child and apply event handlers and 'tooltip' class
-  const childWithProps = cloneElement(children, {
-    className: `${children.props.className || ""} tooltip`.trim(),
-    onMouseEnter: handleMouseEnter,
-    onMouseLeave: handleMouseLeave
-  });
+// export const Tooltip = ({text, children}) => {
 
-  return (
-    <>
-      {childWithProps}
-      {showTooltip && <div className="tooltiptext">{text}</div>}
-    </>
-  );
+//   const [visible, setvisible]=useState(false)
+
+//   return (
+//     <div className='tooltip' onMouseLeave={()=>{setvisible(false)}} onMouseEnter={()=>{setvisible(true)}}>
+//       {children}
+//       {visible && <div className='tooltiptext' >{text}</div>}
+//     </div>
+//   )
+// }
+
+// export default Tooltip
+
+import React, { useState } from 'react';
+
+const Tooltip = ({ text, children }) => {
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <div 
+            className="tooltip" 
+            onMouseEnter={() => setVisible(true)} 
+            onMouseLeave={() => setVisible(false)}
+        >
+            {children}
+            {visible && <div className="tooltiptext">{text}</div>}
+        </div>
+    );
 };
 
-export default ToolTip;
+export default Tooltip;
